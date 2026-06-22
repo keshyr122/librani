3fe9255b424e91e0730d92ced0f3e8b518684cb0
kevin.mora <kevin.mora@north.com>
2026-06-22 06:51:20 -0600

    moves files, new estructure

--- src/server.js
+++ src/server.js
@@ -1,12 +1,15 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-// Le dice a Express que sirva tu HTML y tu JS del front-end
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Servidor de la librería de anime activo en el puerto ${PORT}`);
 });
